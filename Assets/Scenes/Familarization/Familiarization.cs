b19756fa212941898af33972a2141d0c9e4a4e45
Quân <quan@foolography.com>
2021-06-05 20:10:31 +0200

    random position

--- Assets/Scenes/Familarization/Familiarization.cs
+++ Assets/Scenes/Familarization/Familiarization.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Familiarization : MonoBehaviour
@@ -8,9 +9,11 @@ public class Familiarization : MonoBehaviour
     }
     
     public GameObject sampleObject;
+
     void Start()
     {
         Helper.prepareCursors();
+        randomizePosition();
     }
 
     void Update()
@@ -26,6 +29,31 @@ public class Familiarization : MonoBehaviour
             case TrialState.Order:
                 break;
             case TrialState.Trial:
+                break;
+        }
+    }
+
+    private void randomizePosition()
+    {
+        var positions = Enum.GetValues(typeof(Position));
+        var random = new Random();
+        var newPosition = (Position)positions
+            .GetValue(random.Next(positions.Length));
+
+        switch (newPosition)
+        {
+            case Position.Up:
+                // do nothing, since it is already on top from initial
+                sampleObject.transform.position = new Vector2(0, 3);
+                break;
+            case Position.Down:
+                sampleObject.transform.position = new Vector2(0, -3);
+                break;
+            case Position.Left:
+                sampleObject.transform.position = new Vector2(-3, 0);
+                break;
+            case Position.Right:
+                sampleObject.transform.position = new Vector2(3, 0);
                 break;
         }
     }
