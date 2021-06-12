b521e5d0178f44bc0e6fbd10c03d54225f065051
Quân Đinh <urjhams@gmail.com>
2021-06-12 02:03:23 +0200

    trial Familization scene

--- Assets/Scenes/Familarization/Familiarization.cs
+++ Assets/Scenes/Familarization/Familiarization.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 using UnityEngine.SceneManagement;
 
 public class Familiarization : MonoBehaviour
--- Assets/Scenes/Main menu/MenuManager.cs
+++ Assets/Scenes/Main menu/MenuManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -8,5 +9,14 @@ public class MenuManager : MonoBehaviour
     public void saveCurrentName()
     {
         Global.participantName = nameField.text;
+    }
+
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Global.currentState = TrialState.Trial;
+            SceneManager.LoadScene("Familiarization");
+        }
     }
 }
